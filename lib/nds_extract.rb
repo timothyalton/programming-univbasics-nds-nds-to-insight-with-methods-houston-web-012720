bfbdfa65de2fd526b49ff99be97a8f5b8a9f2ca8
Mitchell T Alton <mitchell.alton@live.com>
2020-01-21 22:34:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -39,9 +39,8 @@ def directors_totals(nds)
   result = {}
   director_index = 0
   while director_index < nds.length do
-    director = nds[director_index]
-    name = director[:name]
-    result[name] = gross_for_director(director)
+    director = nds[director_index][:name]
+    result[director] = gross_for_director(director)
     director_index += 1
   end
   result
